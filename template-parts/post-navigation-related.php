f8041d9d7120f02c9dc14bed4d23876a464d636e
’yoshibo488’ <fymytlsb.wand0422@gmail.com>
2025-05-20 09:18:26 +0900

    パンくずリストとnoimage画像の修正

--- template-parts/post-navigation-related.php
+++ template-parts/post-navigation-related.php
@@ -33,7 +33,7 @@
                             <img src="<?php echo esc_url($desktop[0]); ?>" alt="<?php echo esc_attr(get_the_title()); ?>">
                         </picture>
                     <?php else : ?>
-                        <img src="<?php echo esc_url(get_template_directory_uri() . '/images/noimage.jpg'); ?>" alt="noimage">
+                        <img src="<?php echo esc_url(get_template_directory_uri() . '/images/noimage2.jpg'); ?>" alt="noimage">
                     <?php endif; ?>
                 </div>
                 <?php
